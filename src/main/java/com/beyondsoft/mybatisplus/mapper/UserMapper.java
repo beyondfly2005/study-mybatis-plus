fbcacefa90500d3da736a1eafc1685805c2e8bdb
beyondSoft <498601485@qq.com>
2020-09-28 00:30:26 +0800

    study

---
+++ src/main/java/com/beyondsoft/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.beyondsoft.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.beyondsoft.mybatisplus.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
