1146a9df8970f5384cf4668d394cbb63b14ce7b1
beyondSoft <498601485@qq.com>
2020-09-28 00:43:59 +0800

    study

--- src/main/java/com/beyondsoft/mybatisplus/mapper/UserMapper.java
+++ src/main/java/com/beyondsoft/mybatisplus/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package com.beyondsoft.mybatisplus.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.beyondsoft.mybatisplus.entity.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserMapper extends BaseMapper<User> {
 }
--- src/test/java/com/beyondsoft/studymybatisplus/StudyMybatisPlusApplicationTests.java
+++ src/test/java/com/beyondsoft/studymybatisplus/StudyMybatisPlusApplicationTests.java
@@ -1,13 +1,24 @@
 package com.beyondsoft.studymybatisplus;
 
+import com.beyondsoft.mybatisplus.MybatisPlusApplication;
+import com.beyondsoft.mybatisplus.entity.User;
+import com.beyondsoft.mybatisplus.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.List;
+
+@SpringBootTest(classes = MybatisPlusApplication.class)
 class StudyMybatisPlusApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
-    void contextLoads() {
+    void fidAll() {
+        List<User> users = userMapper.selectList(null);
+        System.out.println(users);
     }
 
 }
